a5e8a95a45e508bd466e35e0c77cd8db289cca77
Alex nero <jesalemalo@gmail.com>
2024-05-12 08:37:03 -0400

    feature/create-service blog

--- services/blog_service.go
+++ services/blog_service.go
@@ -1,1 +1,18 @@
 package services
+
+import (
+	"alexnerotd/blog/models"
+	"alexnerotd/blog/repositories"
+)
+
+type BlogService struct {
+	blogRepository *repositories.BlogRepository
+}
+
+func NewBlogService(blogRepository *repositories.BlogRepository) *BlogService {
+	return &BlogService{blogRepository: blogRepository}
+}
+
+func (s *BlogService) GetBlogById(id int) (*models.Blog, error) {
+	return s.blogRepository.GetBlogById(id)
+}
