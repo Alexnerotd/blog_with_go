661ba0e64a18b2976c25d96da5892c9eca9bc97f
Alex nero <jesalemalo@gmail.com>
2024-05-11 22:45:33 -0400

    feature/add-connection with database

--- utils/blog_db.go
+++ utils/blog_db.go
@@ -1,1 +1,46 @@
 package utils
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() (*gorm.DB, error) {
+	// Charge environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbName := os.Getenv("DB_NAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=True", dbUser, dbPassword, dbHost, dbName)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	testDB, err := db.DB()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer testDB.Close()
+
+	err = testDB.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db, nil
+}
