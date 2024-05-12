561a19624907236bd091eec1263d4d2fe7e867f1
Alex nero <jesalemalo@gmail.com>
2024-05-12 08:49:36 -0400

    feature/create-handler blog

--- handlers/blog_handler.go
+++ handlers/blog_handler.go
@@ -1,1 +1,32 @@
 package handlers
+
+import (
+	"alexnerotd/blog/services"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BlogHandler struct {
+	blogService *services.BlogService
+}
+
+func NewBlogHandler(blogService *services.BlogService) *BlogHandler {
+	return &BlogHandler{blogService: blogService}
+}
+
+func (h *BlogHandler) GetBlogById(c *gin.Context) {
+	blogID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid blog ID"})
+		return
+	}
+	blog, err := h.blogService.GetBlogById(blogID)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Blog not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, blog)
+}
